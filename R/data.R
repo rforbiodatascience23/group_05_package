4d911a3771a4ab4695cd4d91a7ea528e73bce7bd
Emmanuel-mg <emmanuel.minois-genin@ecl20.ec-lyon.fr>
2023-10-24 09:19:17 +0200

    Adding codon in the variables

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Codon conversion from Codon to Amino Acid
+#'
+#' A dictionary whose keys are Codons and values are Amino Acids
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon"
